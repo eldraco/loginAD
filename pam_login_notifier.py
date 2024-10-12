9e503b6f0c83d857641e54460406a3bb6ccd297a
Sebas Garcia <eldraco@gmail.com>
2024-10-12 19:22:38 +0200

    Works with pub key and good password (double log), and bad password

--- pam_login_notifier.py
+++ pam_login_notifier.py
@@ -5,6 +5,7 @@ import sys
 from datetime import datetime
 import syslog
 
+
 # Initialize syslog with a specific facility
 syslog.openlog("pam_login_notifier", facility=syslog.LOG_LOCAL7)
 
@@ -50,34 +51,65 @@ if __name__ == "__main__":
 
     login_time = datetime.now().isoformat()  # Get current datetime in ISO format
 
+    # EXtract if pubkey
+    ssh_auth_info = os.environ.get("SSH_AUTH_INFO_0", 'unset').strip()
+
     # Determine the connection type and success
     pam_type = os.environ.get("PAM_TYPE", "unknown")
     connection_type = "unknown"  # Default connection type
     success = False  # Default to failure
+    session_class = os.environ.get("XDG_SESSION_CLASS", "unknown")
 
     # Check if the script is in the auth phase
-    if pam_type == 'auth':
-        pam_result = os.environ.get("PAM_RESULT", "1")  # Default to failure
-        syslog.syslog(syslog.LOG_DEBUG, f'PAM_RESULT: {pam_result}')
-        if pam_result == "0":  # PAM_RESULT 0 indicates success
+    if 'password' in ssh_auth_info:
+        connection_type = "password"  # Assume password login
+        syslog.syslog(syslog.LOG_DEBUG, f'Type of connection identified: password')
+        if pam_type == 'open_session':
+            # When in open_session, assume the user has logged in successfully
             success = True
-            connection_type = "password"  # Assume password login
+            syslog.syslog(syslog.LOG_DEBUG, f'- Succesful: True')
+            # Log received data
+            syslog.syslog(syslog.LOG_DEBUG, f'Received data - Server IP: {server_ip}, Client IP: {client_ip}, Username: {username}, Login Time: {login_time}, Success: {success}, Connection Type: {connection_type}')
+            # Send data to the API only
+            exit_code = send_login_data(server_ip, client_ip, login_time, username, success, connection_type)
+        elif pam_type == 'unknown':
+            success = False
+            syslog.syslog(syslog.LOG_DEBUG, f'- Succesful: False')
+            # Log received data
+            syslog.syslog(syslog.LOG_DEBUG, f'Received data - Server IP: {server_ip}, Client IP: {client_ip}, Username: {username}, Login Time: {login_time}, Success: {success}, Connection Type: {connection_type}')
+            # Send data to the API only
+            exit_code = send_login_data(server_ip, client_ip, login_time, username, success, connection_type)
+    elif 'publickey' in ssh_auth_info:
+        connection_type = "public key"  # Assume public key if authentication fails
+        syslog.syslog(syslog.LOG_DEBUG, f'Type of connection identified: pubkey')
+        if pam_type == 'open_session':
+            # When in open_session, assume the user has logged in successfully
+            success = True
+            syslog.syslog(syslog.LOG_DEBUG, f'- Succesful: True')
+            # Log received data
+            syslog.syslog(syslog.LOG_DEBUG, f'Received data - Server IP: {server_ip}, Client IP: {client_ip}, Username: {username}, Login Time: {login_time}, Success: {success}, Connection Type: {connection_type}')
+            # Send data to the API only
+            exit_code = send_login_data(server_ip, client_ip, login_time, username, success, connection_type)
+        elif pam_type == 'unknown':
+            success = False
+            syslog.syslog(syslog.LOG_DEBUG, f'- Succesful: False')
+    elif 'unset' in ssh_auth_info:
+        if session_class == 'user':
+            # This is the session after a success ful password login. We already logged this. Ignore
+            pass
         else:
-            connection_type = "public key"  # Assume public key if authentication fails
+            connection_type = "password"  # Assume password login
+            syslog.syslog(syslog.LOG_DEBUG, f'Type of connection identified: password')
+            success = False
+            syslog.syslog(syslog.LOG_DEBUG, f'- Succesful: False (SSH Authis unset)')
+            # Log received data
+            syslog.syslog(syslog.LOG_DEBUG, f'Received data - Server IP: {server_ip}, Client IP: {client_ip}, Username: {username}, Login Time: {login_time}, Success: {success}, Connection Type: {connection_type}')
+            # Send data to the API only
+            exit_code = send_login_data(server_ip, client_ip, login_time, username, success, connection_type)
+    elif pam_type == 'close_session':
+        # session is being closed. Do nothing
+        syslog.syslog(syslog.LOG_INFO, "Closed session. Not sending data to API.")
 
-    elif pam_type == 'open_session':
-        # When in open_session, assume the user has logged in successfully
-        success = True
-        connection_type = os.environ.get("SSH_AUTH_INFO_0", "unknown").strip()  # Determine connection type
-
-    # Log received data
-    syslog.syslog(syslog.LOG_DEBUG, f'Received data - Server IP: {server_ip}, Client IP: {client_ip}, Username: {username}, Login Time: {login_time}, Success: {success}, Connection Type: {connection_type}')
-
-    # Send data to the API only if success is determined
-    if success:
-        exit_code = send_login_data(server_ip, client_ip, login_time, username, success, connection_type)
-    else:
-        syslog.syslog(syslog.LOG_INFO, "Authentication failed or not in auth phase. Not sending data to API.")
 
     # Exit with appropriate code
     sys.exit(exit_code if success else 0)
